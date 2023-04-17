5f35c16c6954e0d574d0bc6f1551067209f370be
*LMwqQC9QCB <loanntk@newit.co.jp>
2023-04-17 17:06:34 +0700

    17.4.2023

--- utils/getContractProvider.ts
+++ utils/getContractProvider.ts
@@ -4,7 +4,7 @@ export const getContractProvider = async () => {
   const ADDRESS = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS
   const contractAbi = require('../abi/contract.abi.json')
   const provider = new ethers.providers.InfuraProvider(
-    process.env.NEXT_PUBLIC_TEST_MINT ? 'sepolia' : 'homestead'
+    process.env.NEXT_PUBLIC_TEST_MINT ? 'goerli' : 'homestead'
   )
   const icoContract = ADDRESS && new ethers.Contract(ADDRESS, contractAbi, provider)
   return icoContract
