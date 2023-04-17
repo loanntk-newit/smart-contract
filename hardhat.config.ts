048c0b3c8b79708980605c6924aac9b683e49388
Kim Loan <haji98.hjjk@gmail.com>
2023-04-18 00:50:49 +0700

    update contract 18.04.2023

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,8 +6,8 @@ const NETWORK = "goerli";
 // const NETWORK = "mainnet";
 const INFURA_API_KEY = "a4e894db57a74d9ea47e4fe50cf0f095";
 const PRIVATE_KEY =
-  // "471f4bddc870b6302918bcf9aae77954cae14bf11730a60bfdae62a4aa5390f1";
-  "b7d2f37d475830013e1f1fe8858686e43cebc6d16f88df7de2db4ae4e741f6e4";
+  "471f4bddc870b6302918bcf9aae77954cae14bf11730a60bfdae62a4aa5390f1";
+  // "b7d2f37d475830013e1f1fe8858686e43cebc6d16f88df7de2db4ae4e741f6e4";
 const ETHERSCAN_API_KEY = "5MK5SDTQ25QQ1AI9EHZNH73N3TYZQ2FQXC";
 
 const config: HardhatUserConfig = {
